8bdbe3cb359851bf20b0186c0aa594cad6967e84
Oksana <oksanakysylychak@gmail.com>
2025-03-23 10:30:00 +0200

    hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
-import MyComponents from "./MyComponents/myComponents.tsx";
-import myComponents from "./MyComponents/myComponents.tsx";
 
 function App() {
 
   return (
     <>
-      <MyComponents text={'hello-1'}/>
-        {myComponents({text:'hello-2'})}
+
     </>
   )
 }
