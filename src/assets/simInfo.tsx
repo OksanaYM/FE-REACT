2bba3c4b280d418157232d79bcbe0e1ab49102f2
Oksana <oksanakysylychak@gmail.com>
2025-03-24 15:10:52 +0200

    hw

---
+++ src/assets/simInfo.tsx
@@ -0,0 +1,14 @@
+import {simpsons} from "./sim.ts";
+
+
+export const SimInfo = () => {
+    return(
+        <ul>
+            {
+                simpsons.map((value, index) => {
+                    return
+                })
+            }
+        </ul>
+    )
+}
---
+++ src/character/sim.tsx
@@ -0,0 +1,4 @@
+
+type SimType ={
+    info:
+}
