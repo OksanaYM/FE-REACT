257d2f1daa6db0e072cb29870661cc004b5e5fe6
Oksana <oksanakysylychak@gmail.com>
2025-03-24 12:11:48 +0200

    hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+// #gi01MZ6v   з arrays.js взяти масив coursesTitleArray. Вивести кожен його елемент, створивши необхідні моделі та компоненти
+
 import './App.css'
 
-function App() {
+const coursesTitleArray: string[] = [
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'Fullstack',
+  'Frontend'
+];
+
+const App = () => {
 
   return (
-    <>
+      <>
+        <ul>
+          {
+            coursesTitleArray.map((value , index)=> <li key={index}>{value}</li> )
+          }
+        </ul>
 
-    </>
+      </>
   )
 }
 
 export default App;
+
