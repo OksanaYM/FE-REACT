5cbc46a8fed8465bba078d347baa62208376fb7b
Oksana <oksanakysylychak@gmail.com>
2025-03-26 14:07:40 +0200

    hw

--- src/components/post/postComponent.tsx
+++ src/components/post/postComponent.tsx
@@ -1,9 +1,10 @@
 import {IModelPost} from "../../models/modelPost.ts";
 import {FC} from "react";
+import './post.css'
 
 export const Post: FC<{post: IModelPost}> = ({post:{id, userId, title, body}}) => {
     return(
-        <div>
+        <div className={'main'}>
             <h2>{id}</h2>
             <h4>{userId} Title: {title} </h4>
             <p>{body}</p>
