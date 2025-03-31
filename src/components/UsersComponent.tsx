16a5d0ba6e5cd6905807bc9d2ac495b436fbb19a
Oksana <oksanakysylychak@gmail.com>
2025-03-31 13:47:31 +0300

    hw

---
+++ src/components/UsersComponent.tsx
@@ -0,0 +1,8 @@
+export const UsersComponent =() =>{
+
+
+    return(
+        <>
+        </>
+    )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import {MainLayout} from "../Layout/Layout.tsx";
+import {UsersPage} from "../Pages/UsersPage.tsx";
 
 export const routes = createBrowserRouter( [ {
     path: '/', element: <MainLayout/>, children:[
