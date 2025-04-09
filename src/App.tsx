3439f044c2a07dbdda67c38d3aa458bc1141e579
Oksana <oksanakysylychak@gmail.com>
2025-04-09 15:51:33 +0300

    hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import {useFetch} from "./hook/useFetch.tsx";
 
 function App() {
+  const users =
+      useFetch<{id: number, name: string}[]>('https://jsonplaceholder.typicode.com/users', [])
 
   return (
-    <>
+    <>{
+      users.map((user) =>(
+          <div key={user.id}>
+            {user.id} - {user.name}
+          </div>
+      ))
+    }
 
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,12 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(<App/>);
+
+//
+// Створити кастомний  хук  який буде повертати відповіді з апі і заміняти useState+ useEffect. ендпоінт апі –
+// аргумент вашого хука. Продумати логіку типізації відповіді.
+//
+//     Приклад сигнатури:
+//
+//     useFetch<T>(url)
+//
