61bbacb92793e11af19acb7cbc9a1b86ca11b3a8
Oksana <oksanakysylychak@gmail.com>
2025-04-18 14:13:50 +0300

    hw

--- src/Components/ComplexesComponent.tsx
+++ src/Components/ComplexesComponent.tsx
@@ -3,6 +3,12 @@ import {useEffect} from "react";
 import {userActions} from "../Slices/UserSlice.ts";
 import {postAction} from "../Slices/PostSlice.ts";
 import {commentAction} from "../Slices/CommentSlice.ts";
+import {IUser} from "../Models/IUser.ts";
+import {UserComponent} from "../Component/UserComponent.tsx";
+import {IPost} from "../Models/IPost.ts";
+import {PostComponent} from "../Component/PostComponent.tsx";
+import {IComment} from "../Models/IComment.ts";
+import {CommentComponent} from "../Component/CommentComponent.tsx";
 
 export const ComplexesComponent = () =>{
     const dispatch= useAppDispatch()
@@ -20,6 +26,12 @@ export const ComplexesComponent = () =>{
     }, []);
     return(
         <>
+            {users.map((user:IUser)  =>
+                <UserComponent key={user.id} user={user} />)}
+            {posts.map((post:IPost) =>
+                <PostComponent key={post.id} post={post}/>)}
+            {comments.map((comment:IComment) =>
+                <CommentComponent key={comment.id} comment={comment}/>)}
         </>
     )
 }
