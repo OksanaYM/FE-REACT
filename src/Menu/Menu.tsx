123b5994af39306cd9b5b4904dde421577f543cd
Oksana <oksanakysylychak@gmail.com>
2025-04-23 12:44:06 +0300

    hw

---
+++ src/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+export const Mtnu =() =>{
+    return(
+        <div>
+            <ul>
+                <li>
+                    <Link to={}
+                </li>
+            </ul>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(<BrowserRouter router={routes}/>);
+createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>);
 
